2214b3b31e522802f580dfd6f224d8f285024ffd
rahucqu <12245395@cqumail.com>
2025-09-17 23:37:23 +0600

    add agent factory

---
+++ database/factories/AgentFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\Agent;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Agent>
+ */
+class AgentFactory extends Factory
+{
+    protected $model = Agent::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'phone' => fake()->phoneNumber(),
+            'bio' => fake()->paragraphs(2, true),
+            'avatar' => null,
+        ];
+    }
+}
