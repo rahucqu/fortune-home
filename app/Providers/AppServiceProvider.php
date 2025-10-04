7be74058cc09a61b7e10c2ed8e696d3fe0fee743
rahucqu <12245395@cqumail.com>
2025-10-04 23:07:51 +0600

    update app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,12 +4,26 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use App\Models\User;
-use App\Observers\UserObserver;
+use App\Models\BlogComment;
+use App\Models\BlogEditRequest;
+use App\Models\BlogPost;
+use App\Policies\BlogCommentPolicy;
+use App\Policies\BlogEditRequestPolicy;
+use App\Policies\BlogPostPolicy;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * The policy mappings for the application.
+     */
+    protected array $policies = [
+        BlogPost::class => BlogPostPolicy::class,
+        BlogComment::class => BlogCommentPolicy::class,
+        BlogEditRequest::class => BlogEditRequestPolicy::class,
+    ];
+
     /**
      * Register any application services.
      */
@@ -23,6 +37,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        User::observe(UserObserver::class);
+        $this->registerPolicies();
+    }
+
+    /**
+     * Register the application's policies.
+     */
+    public function registerPolicies(): void
+    {
+        foreach ($this->policies as $model => $policy) {
+            Gate::policy($model, $policy);
+        }
     }
 }
